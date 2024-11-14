2b0b8b1aeb6cae73a55e1a8c61bbcf4007f450db
Ingridoliveira <oligrid20@gmail.com>
2024-11-14 14:14:21 -0300

    feat(db): criar tabela Tb_User

---
+++ src/models/user.js
@@ -0,0 +1,42 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/conection";
+
+export const User = sequelize.define(
+  "Tb_User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    fistname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    surname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Tabelas sincronizadas.");
+  })
+  .catch((err) => {
+    console.error("Erro ao sincronizar tabelas:", err);
+  });
