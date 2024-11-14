4e65db2f7b2625a779b51f81cdb7ba442bc5423c
Ingridoliveira <oligrid20@gmail.com>
2024-11-14 14:14:58 -0300

    fix: .js

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,5 @@
 import { DataTypes } from "sequelize";
-import { sequelize } from "../config/conection";
+import { sequelize } from "../config/conection.js";
 
 export const User = sequelize.define(
   "Tb_User",
