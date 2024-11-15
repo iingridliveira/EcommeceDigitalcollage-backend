2917209034aaf8ca79f24dc1f20ba23c50b37bdd
Ingridoliveira <oligrid20@gmail.com>
2024-11-15 12:54:23 -0300

    fix: respostas tratadas

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,17 +3,35 @@ import { UserServices } from "../services/user.service.js";
 const instaciaservece = new UserServices();
 
 const createUser = async (req, res) => {
-  const {  firstname, surname, email, password } = req.body;
-  const newUser = await instaciaservece.registerService(
-    firstname,
-    surname,
-    email,
-    password
-  );
+  try {
+    const { firstname, surname, email, password } = req.body;
+    const newUser = await instaciaservece.registerService(
+      firstname,
+      surname,
+      email,
+      password
+    );
 
-  return res.status(201).json({
-    message: "User criado ",
-    newUser,
-  });
+    return res.status(201).json({
+      message: "User criado ",
+      newUser,
+    });
+  } catch (error) {
+     if (error.response) {
+      // Erro da API consumida
+      res
+        .status(error.response.status)
+        .json({ error: error.response.data || "Erro na API externa" });
+    } else if (error.request) {
+      // Sem resposta da API consumida
+      res.status(504).json({ error: "Gateway Timeout: Sem resposta do servidor externo." });
+    } else {
+      // Outros erros
+      res.status(500).json({ error: "Erro interno no servidor: " + error.message });
+    }
+  
+    
+  }
+  
 };
 export { createUser };
