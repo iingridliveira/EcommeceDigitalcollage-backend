394464880a0ac335fd663f5b3b5a6774caae83ec
franciscojose96 <franciscojoserodrigueslins@gmail.com>
2024-11-14 23:43:47 -0300

    tabela de product

--- src/models/product.js
+++ src/models/product.js
@@ -1,13 +1,17 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/conection.js";
 
-export const Category = sequelize.define(
-    "Tb_Category",
+export const Product = sequelize.define(
+    "Tb_Product",
     {
         id: {
             type: DataTypes.INTEGER,
             autoIncrement: true,
             primaryKey: true,
+        },
+        enabled: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: 0,
         },
         name: {
             type: DataTypes.STRING,
@@ -22,9 +26,26 @@ export const Category = sequelize.define(
             allowNull: true,
             defaultValue: 0,
         },
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            defaultValue: 0,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        price: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        },
+        price_with_discount: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        },
     },
     {
-        timestamps: true,
+        timestamps:true,
     }
 );
 
