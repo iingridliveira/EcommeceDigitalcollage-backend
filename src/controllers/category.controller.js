2700295b9bb437f9cab093f447dd844b3f7d9ace
franciscojose96 <franciscojoserodrigueslins@gmail.com>
2024-11-27 23:55:48 -0300

    Update category

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -32,4 +32,34 @@ const createCategory = async (req, res) => {
     }
   }
 };
-export {createCategory};
+
+const updateCategory = async (req, res) => {
+   try {
+    const {id} = req.params;
+  const {name, slug, use_in_menu} = req.body;
+  const newCategoryUpdate = await instaciaService.updateCategory(id, name, slug, use_in_menu)
+  return res.status(201).json({
+    message: "Category atualizada",
+    newCategoryUpdate,
+  });
+  } catch (error) {
+    if (error.response) {
+      // Erro da API consumida
+      res
+        .status(error.response.status)
+        .json({ error: error.response.data || "Erro na API externa" });
+    } else if (error.request) {
+      // Sem resposta da API consumida
+      res
+        .status(504)
+        .json({ error: "Gateway Timeout: Sem resposta do servidor externo." });
+    } else {
+      // Outros erros
+      res
+        .status(500)
+        .json({ error: "Erro interno no servidor: " + error.message });
+    }
+  }
+}
+
+export {createCategory, updateCategory};
--- src/services/category.service.js
+++ src/services/category.service.js
@@ -1,3 +1,4 @@
+import { where } from "sequelize";
 import { Category } from "../models/category.js";
 
 export class CategoryService {
@@ -8,5 +9,14 @@ export class CategoryService {
       use_in_menu,
     });
     return category;
+  }
+  async updateCategory(id, name, slug, use_in_menu) {
+    const idCategory = await Category.update({
+      name,
+      slug,
+      use_in_menu,
+    },
+    {where: {id}})
+    return idCategory;
   }
 }
